12658e05b906667137327be778a5e130bc981d7b
Naga Deepak Kaza <83226719+nagadeepak511@users.noreply.github.com>
2021-08-15 17:00:47 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,3 @@
+function test(){
+			document.getElementById("output").innerText = "The answer is: " + (parseInt(document.getElementById("first").value) + parseInt(document.getElementById("second").value))
+		}
