60d34dd576c6e36606799bee3c2053393e344d34
Naga Deepak Kaza <83226719+nagadeepak511@users.noreply.github.com>
2021-08-16 20:38:02 +0530

    Delete calc.js

--- calc.js
+++
@@ -1,3 +0,0 @@
-function test(){
-			document.getElementById("output").innerText = "The answer is: " + (parseInt(document.getElementById("first").value) + parseInt(document.getElementById("second").value))
-		}
